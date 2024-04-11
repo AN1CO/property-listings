7b67004e9b19f8dbb223d65ce3d2beaa67019ef3
an1co <skye.anico@gmail.com>
2024-04-10 21:08:50 -0400

    removed separate component for like btn

--- src/components/LikeButton/LikeButton.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-import heartFill from '../../assets/heart-fill.svg';
-import heartStroke from '../../assets/heart-stroke.svg';
-
-const LikeButton = ({ mlsId }) => {
-  return <>something</>;
-};
-
-export default LikeButton;
--- src/components/LikeButton/LikeButton.test.jsx
+++
@@ -1,4 +0,0 @@
-import {cleanup, fireEvent, render} from '@testing-library/react';
-import LikeButton from './LikeButton'
-
-test("LikeButton should change svg image when clicked", () => {})
